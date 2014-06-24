f6b9cf9656f2478fa0db4d520f9f2a4a46e76aa0
ggona11 <ggona11@freeuni.edu.ge>
2014-06-25 01:04:04 +0400

    categoryManager

--- src/model/CategoryManager.java
+++ src/model/CategoryManager.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 public class CategoryManager extends Manager {
 	static String tableName = "categories";
 
-	public ArrayList<String> getAllCategories(Connection con) {
+	public static ArrayList<String> getAllCategories(Connection con) {
 		ArrayList<ArrayList<Object>> ar = getAllRows(con, tableName, 2);
 		ArrayList<String> res = new ArrayList<String>();
 		for (int i = 0; i < ar.size(); i++) {
@@ -15,12 +15,16 @@ public class CategoryManager extends Manager {
 		return res;
 	}
 	
-	public ArrayList<Integer> getCategoryIDs(Connection con){
+	public static ArrayList<Integer> getCategoryIDs(Connection con){
 		ArrayList<ArrayList<Object>> ar = getAllRows(con, tableName, 2);
 		ArrayList<Integer> res = new ArrayList<Integer>();
 		for (int i = 0; i < ar.size(); i++) {
 			res.add((Integer) ar.get(i).get(0));
 		}
 		return res;
+	}
+	
+	public static String getCategoryByID(Connection con, int ID){
+		return (String)getSingleString(con, tableName, "ID", ID, "type");
 	}
 }
